75f2e92d5926f80a28846e81a269aa69a28b3ab8
kishanhitk <kishansharma1231@gmail.com>
2021-08-01 13:39:04 +0530

    ✨ Add angle possibility checker

--- pages/angle.tsx
+++ pages/angle.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
-import React from "react";
+import React, { FormEvent } from "react";
 
 const AnglePage = () => {
-  const [a, setA] = React.useState(0);
-  const [b, setB] = React.useState(0);
-  const [c, setC] = React.useState(0);
+  const [a, setA] = React.useState(null);
+  const [b, setB] = React.useState(null);
+  const [c, setC] = React.useState(null);
+  const [output, setOutput] = React.useState("Output");
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (a + b + c === 180) {
+      setOutput("Yes, triangle is possible");
+    } else {
+      setOutput("No, triangle is impossible");
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -21,20 +30,45 @@ const AnglePage = () => {
             Enter the angles in below input boxes and we will tell you if those
             angles make a Triangle
           </p>
-          <form>
-            <label>
-              <input
-                className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7  sm:text-sm border-purple-900 rounded-md"
-                type="number"
-                name="a"
-                placeholder="Enter the angle A"
-                value={a}
-                onChange={(e) => setA(Number(e.target.value))}
-              />
-            </label>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="my-5 focus:ring-indigo-500 focus:border-indigo-500 block w-full   sm:text-sm border-purple-900 bg-purple-200 p-5 rounded-md"
+              type="number"
+              min="0"
+              max="180"
+              name="a"
+              placeholder="angle A"
+              value={a}
+              onChange={(e) => setA(Number(e.target.value))}
+            />
+            <input
+              className="my-5 focus:ring-indigo-500 focus:border-indigo-500 block w-full   sm:text-sm border-purple-900 bg-purple-200 p-5 rounded-md"
+              type="number"
+              name="a"
+              min="0"
+              max="180"
+              placeholder="angle B"
+              value={b}
+              onChange={(e) => setB(Number(e.target.value))}
+            />
+            <input
+              className="my-5 focus:ring-indigo-500 focus:border-indigo-500 block w-full   sm:text-sm border-purple-900 bg-purple-200 p-5 rounded-md"
+              type="number"
+              name="a"
+              min="0"
+              max="180"
+              placeholder="angle C"
+              value={c}
+              onChange={(e) => setC(Number(e.target.value))}
+            />
             <br />
-            <button type="submit">Submit</button>
-            <div>Output</div>
+            <button
+              className="bg-blue-700 px-5 py-2 shadow-md hover:shadow-sm hover:bg-blue-600 rounded-lg text-white"
+              type="submit"
+            >
+              Submit
+            </button>
+            <div className="mt-4 text-blue-700 text-lg">{output}</div>
           </form>
         </div>
       </main>
